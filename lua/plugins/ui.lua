58cf1879db48b576fb2b7f636dd09e19c8951d5e
alvinceleste <alvarogrbl@gmail.com>
2024-10-04 18:19:48 +0200

    Lualine things and assets

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -366,15 +366,15 @@ return {
             },
           },
           lualine_y = {
-            { "encoding" },
+            -- { "encoding" },
             { "progress", separator = " ", padding = { left = 1, right = 0 } },
             { "location", padding = { left = 0, right = 1 } },
           },
-          lualine_z = {
-            function()
-              return " " .. os.date("%R")
-            end,
-          },
+          --lualine_z = {
+          --function()
+          --return " " .. os.date("%R")
+          --end,
+          --},
         },
         extensions = { "neo-tree", "lazy" },
       }
